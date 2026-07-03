d930547af49fa72291ce4ed7d1472bd223b26765
Ashlesh Bathina <ashleshbathina@gmail.com>
2026-07-03 17:13:56 +0000

    refactor: remove debug log from root route handler

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import pool from "./configs/db.js";
 const app = express();
 
 app.get('/', async (req, res) => {
-  console.log('hit this--------------------')
   const data = await pool.query('SELECT * FROM owner');
   console.log(data.rows);
   return {data};
